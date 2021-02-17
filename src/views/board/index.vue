1923fee4cc979ea58c7415b13b34173f37f0813f
kmwstory <kmwstory@gmail.com>
2021-02-17 09:04:40 +0900

    32 commit

--- src/views/board/index.vue
+++ src/views/board/index.vue
@@ -5,14 +5,19 @@
       :headers="headers"
       :items="items"
       :items-per-page="5"
+      :options.sync="options"
+      :server-items-length="serverItemsLength"
+      must-sort
     >
       <template v-slot:item.id="{ item }">
         <v-btn icon @click="openDialog(item)"><v-icon>mdi-pencil</v-icon></v-btn>
         <v-btn icon @click="remove(item)"><v-icon>mdi-delete</v-icon></v-btn>
       </template>
+      <template v-slot:item.createdAt="{ item }">
+        {{item.createdAt.toLocaleString()}}
+      </template>
     </v-data-table>
     <v-card-actions>
-      <v-btn @click="read"><v-icon left>mdi-page-next</v-icon></v-btn>
       <v-btn @click="openDialog(null)"><v-icon left>mdi-pencil</v-icon></v-btn>
     </v-card-actions>
     <v-dialog max-width="500" v-model="dialog">
@@ -33,13 +38,16 @@
   </v-card>
 </template>
 <script>
+import { head, last } from 'lodash'
+
 export default {
   data () {
     return {
       headers: [
+        { value: 'createdAt', text: '작성일' },
         { value: 'title', text: '제목' },
         { value: 'content', text: '내용' },
-        { value: 'id', text: 'id' }
+        { value: 'id', text: 'id', sortable: false }
       ],
       items: [],
       form: {
@@ -48,26 +56,66 @@ export default {
       },
       dialog: false,
       selectedItem: null,
-      unsubscribe: null
+      unsubscribe: null,
+      unsubscribeCount: null,
+      serverItemsLength: 0,
+      options: {
+        sortBy: ['createdAt'],
+        sortDesc: [true]
+      },
+      docs: []
+    }
+  },
+  watch: {
+    options: {
+      handler (n, o) {
+        const arrow = n.page - o.page
+        this.subscribe(arrow)
+      },
+      deep: true
     }
   },
   created () {
-    this.subscribe()
+    // this.read()
+
   },
   destroyed () {
     if (this.unsubscribe) this.unsubscribe()
+    if (this.unsubscribeCount) this.unsubscribeCount()
   },
   methods: {
-    subscribe () {
-      this.unsubscribe = this.$firebase.firestore().collection('boards').onSnapshot((sn) => {
+    subscribe (arrow) {
+      this.unsubscribeCount = this.$firebase.firestore().collection('meta').doc('boards').onSnapshot((doc) => {
+        if (!doc.exists) return
+        this.serverItemsLength = doc.data().count
+      })
+      const order = head(this.options.sortBy)
+      const sort = head(this.options.sortDesc) ? 'desc' : 'asc'
+      const limit = this.options.itemsPerPage
+
+      const ref = this.$firebase.firestore().collection('boards').orderBy(order, sort)
+      let query
+      switch (arrow) {
+        case -1: query = ref.endBefore(head(this.docs)).limitToLast(limit)
+
+          break
+        case 1: query = ref.startAfter(last(this.docs)).limit(limit)
+          break
+
+        default: query = ref.limit(limit)
+          break
+      }
+
+      this.unsubscribe = query.onSnapshot((sn) => {
         if (sn.empty) {
           this.items = []
           return
         }
+        this.docs = sn.docs
         this.items = sn.docs.map(v => {
           const item = v.data()
           return {
-            id: v.id, title: item.title, content: item.content
+            id: v.id, title: item.title, content: item.content, createdAt: item.createdAt.toDate()
           }
         })
       })
@@ -84,23 +132,15 @@ export default {
       }
     },
     add () {
-      this.$firebase.firestore().collection('boards').add(this.form)
+      const item = {}
+      Object.assign(item, this.form)
+      item.createdAt = new Date()
+      this.$firebase.firestore().collection('boards').add(item)
       this.dialog = false
     },
     update () {
       this.$firebase.firestore().collection('boards').doc(this.selectedItem.id).update(this.form)
       this.dialog = false
-    },
-    async read () {
-      const sn = await this.$firebase.firestore().collection('boards').get()
-      // if (sn.)
-      this.items = sn.docs.map(v => {
-        const item = v.data()
-        return {
-          id: v.id, title: item.title, content: item.content
-        }
-      })
-      // console.log(this.items)
     },
     remove (item) {
       this.$firebase.firestore().collection('boards').doc(item.id).delete()
